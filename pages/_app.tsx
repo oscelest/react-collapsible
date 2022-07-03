05f6b8d0f98856e947b10c41be41be5184eb48a1
oscelest <oscelest@gmail.com>
2022-07-03 21:03:08 +0200

    Removing next app and cleaning up files.

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../public/style/globals.scss";
-import type {AppProps} from "next/app";
-import React from "react";
-
-function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
