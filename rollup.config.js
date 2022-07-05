6f5562ac217abbae959a5a74e88513814606d81a
oscelest <oscelest@gmail.com>
2022-07-05 23:27:16 +0200

    Small fixes

--- rollup.config.js
+++ rollup.config.js
@@ -3,45 +3,39 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
+import {terser} from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-//NEW
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
-const packageJson = require("./package.json");
+const pkg = require("./package.json");
 
 export default [
   {
-    input: "src/index.ts",
-    output: [
+    input:   "src/index.ts",
+    output:  [
       {
-        file: packageJson.main,
-        format: "cjs",
+        file:      pkg.main,
+        format:    "cjs",
         sourcemap: true,
       },
       {
-        file: packageJson.module,
-        format: "esm",
+        file:      pkg.module,
+        format:    "esm",
         sourcemap: true,
       },
     ],
     plugins: [
-      // NEW
       peerDepsExternal(),
-
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.build.json" }),
+      typescript({tsconfig: "./tsconfig.build.json"}),
       postcss(),
-
-      // NEW
       terser(),
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/types/index.d.ts", format: "esm" }],
-    plugins: [dts()],
+    input:    "dist/esm/types/index.d.ts",
+    output:   [{file: "dist/types/index.d.ts", format: "esm"}],
+    plugins:  [dts()],
     external: [/\.css$/],
   },
 ];
