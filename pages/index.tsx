7765757a04e714200a265b6a32702311a3ba20b7
oscelest <oscelest@gmail.com>
2022-08-09 15:30:30 +0200

    Ready for v0.3.0 - Update to include the possibility of width and height collapsing. - Updated to include a variable for animation speed. - Collapsible can now prioritize going for width or height first.

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Collapsible from "../src/components/Collapsible";
+import CollapsibleDirection from "../src/enums/CollapsibleDimension";
+
+export default function IndexPage() {
+
+  return (
+    <div>
+      <Collapsible direction={CollapsibleDirection.BOTH} label={"Hello World"}>
+        <h1>This is the story of the world</h1>
+        <span>The end</span>
+      </Collapsible>
+    </div>
+  );
+
+}
---
+++ src/enums/CollapsibleDimension.ts
@@ -0,0 +1,9 @@
+enum CollapsibleDirection {
+  WIDTH             = "width",
+  HEIGHT            = "height",
+  BOTH              = "both",
+  BOTH_WIDTH_FIRST  = "wboth",
+  BOTH_HEIGHT_FIRST = "hboth",
+}
+
+export default CollapsibleDirection;
