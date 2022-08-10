c583acc5f0f8c9fe5dbf9413bdf39d9ccc33d107
oscelest <oscelest@gmail.com>
2022-08-10 13:26:25 +0200

    Ready for v0.4.0: - Collapsible state can be controlled from the outside.

--- src/components/Collapsible.tsx
+++ src/components/Collapsible.tsx
@@ -3,18 +3,19 @@ import Style from "./Collapsible.module.css";
 import CollapsibleDirection from "../enums/CollapsibleDimension";
 
 function Collapsible(props: CollapsibleProps) {
-  const {style = {}, label = "\u00A0", direction = CollapsibleDirection.HEIGHT, speed = 200, ...component_method_props} = props;
-  const {onClick, ...component_props} = component_method_props;
+  const {style = {}, label = "\u00A0", collapsed, direction = CollapsibleDirection.HEIGHT, speed = 200, ...component_method_props} = props;
+  const {onClick, onChange, ...component_props} = component_method_props;
 
-  const [collapsed, setCollapsed] = useState<boolean>(true);
+  const [internal_collapsed, setInternalCollapsed] = useState<boolean>(true);
   const [max_width, setMaxWidth] = useState<number>(0);
   const [max_height, setMaxHeight] = useState<number>(0);
   const ref_element = useRef<HTMLInputElement>(null);
 
   style["--collapsible-speed"] = `${speed}ms`;
-  style["--collapsible-max-width"] = `${collapsed && direction !== CollapsibleDirection.HEIGHT ? 0 : max_width}px`;
-  style["--collapsible-max-height"] = `${collapsed && direction !== CollapsibleDirection.WIDTH ? 0 : max_height}px`;
+  style["--collapsible-max-width"] = `${internal_collapsed && direction !== CollapsibleDirection.HEIGHT ? 0 : max_width}px`;
+  style["--collapsible-max-height"] = `${internal_collapsed && direction !== CollapsibleDirection.WIDTH ? 0 : max_height}px`;
 
+  useEffect(() => { collapsed !== undefined && setInternalCollapsed(collapsed);}, [collapsed]);
   useEffect(
     () => {
       const element = ref_element.current?.querySelector(".collapsible-content");
@@ -32,7 +33,7 @@ function Collapsible(props: CollapsibleProps) {
   if (props.className) classes.push(props.className);
 
   return (
-    <div {...component_props} ref={ref_element} className={classes.join(" ")} style={style} data-collapsed={collapsed} data-direction={direction}>
+    <div {...component_props} ref={ref_element} className={classes.join(" ")} style={style} data-collapsed={internal_collapsed} data-direction={direction}>
       <div className={"collapsible-label"} onClick={onComponentClick}>{label}</div>
       <div className={"collapsible-content"}>
         {props.children}
@@ -42,7 +43,9 @@ function Collapsible(props: CollapsibleProps) {
 
   function onComponentClick(event: React.MouseEvent<HTMLDivElement>) {
     onClick?.(event);
-    if (!event.defaultPrevented) setCollapsed(!collapsed);
+    if (event.defaultPrevented) return;
+    onChange?.(!internal_collapsed)
+    setInternalCollapsed(!internal_collapsed);
   }
 
   function updateElementSize(element: Element) {
@@ -57,11 +60,14 @@ interface CollapsibleStyleProps extends CSSProperties {
   "--collapsible-max-height"?: `${number}px`;
 }
 
-export interface CollapsibleProps extends Omit<HTMLAttributes<HTMLDivElement>, "style"> {
-  direction: CollapsibleDirection;
+export interface CollapsibleProps extends Omit<HTMLAttributes<HTMLDivElement>, "style" | "onChange"> {
+  collapsed?: boolean;
+  direction?: CollapsibleDirection;
   label?: React.ReactNode;
   style?: CollapsibleStyleProps;
   speed?: number;
+
+  onChange(value: boolean): void;
 }
 
 export default Collapsible;
